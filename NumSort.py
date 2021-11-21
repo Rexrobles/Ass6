ae3733e9de6d11b260551d8f3ebfae3f2e429410
Rex-Imman Robles <reximman03@gmail.com>
2021-11-21 21:44:47 +0800

    if first number is the highest number

--- NumSort.py
+++ NumSort.py
@@ -10,3 +10,7 @@ Num1 = float(input("Enter your first number: "))
 Num2 = float(input("Enter your second number: "))
 Num3 = float(input("Enter your third number: "))
 Num4 = float(input("Enter your fourth number: "))
+
+# possible outcome if the first number is the highest number
+if Num1 >= Num2 >= Num3 >= Num4:
+    print (f"Highest to Lowest: {Num1}, {Num2}, {Num3}, {Num4} ")
