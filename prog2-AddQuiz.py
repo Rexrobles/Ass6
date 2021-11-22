f0569e08ada8e99f8a20d5c91fdcb15eb89b346a
Rex-Imman Robles <reximman03@gmail.com>
2021-11-23 03:41:01 +0800

    input from the user

---
+++ prog2-AddQuiz.py
@@ -0,0 +1,8 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+user = input("Hi! Please enter your name to proceed: ")
+print(f"Hi {user}! You are going to answer 10 random Question. Lets get started! ")
