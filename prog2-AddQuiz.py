1a69639d4376eba28d30a5df007a2b8c8f60602d
Rex-Imman Robles <reximman03@gmail.com>
2021-11-23 03:55:32 +0800

    generate two random numbers to add from (0 to 99)

--- prog2-AddQuiz.py
+++ prog2-AddQuiz.py
@@ -6,3 +6,12 @@
 
 user = input("Hi! Please enter your name to proceed: ")
 print(f"Hi {user}! You are going to answer 10 random Question. Lets get started! ")
+
+import random
+# generate two random numbers to add from (0 to 99)
+def randomAdd():
+    Num1 = random.randint(0,99)
+    Num2 = random.randint(0,99)
+    RanAnws = Num1 + Num2
+    print(f"What will be the sum if {Num1} is added to {Num2}?")
+    return RanAnws
